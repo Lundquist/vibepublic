81ca9d32ab477436421b1f61b0f6460161db12a4
Lundquist <Robert.MLundquist@gmail.com>
2020-07-23 15:31:20 +0200

    No times image

--- src/booking/SelectDate/DateItem/index.js
+++ src/booking/SelectDate/DateItem/index.js
@@ -4,10 +4,9 @@ import './DateItem.scss'
 
 const DateItem = (props) => {
     const { date, bookingTimes } = props;
-    console.log("DateItem " + JSON.stringify(bookingTimes))
     return (
         <div className='__date__item'>
-            {bookingTimes.length !== 0 ? bookingTimes.map((time, i) => <div key={i} className='__booking__time'  onClick={() =>props.setDateTime(date, time)}>{time}</div>) : <div>No times available this day</div>}
+            {bookingTimes.map((time, i) => <div key={i} className='__booking__time'  onClick={() =>props.setDateTime(date, time)}>{time}</div>)}
         </div>
     )
 }
