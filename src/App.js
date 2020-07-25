b1712b9641046b8be7e3ade6f29d474d062896ec
Hiwa Farahnoosh <hiwa@redhiro.com>
2020-07-25 08:54:03 +0200

    Dynamic Header

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import './App.css';
 import Booking from './booking'
 import reducer from './store/reducers'
 import { createStore } from 'redux';
+import {Helmet} from "react-helmet";
+import { useSelector } from 'react-redux';
+
+ 
 //const store = createStore(reducer);
 
 const App = (props) => {
+  const { information } = useSelector(({ global }) => global.company);
+
   return (
     <Provider store={store}>
+          <Helmet>
+            <title>{information.name}</title>
+            </Helmet>
       <Booking />
     </Provider>
   );
