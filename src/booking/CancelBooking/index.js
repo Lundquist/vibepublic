bfdf62becbebd6626cf1052043063c4d65fe24db
Hiwa Farahnoosh <hiwa@redhiro.com>
2020-07-25 16:01:11 +0200

    Design for cancelBooking

--- src/booking/CancelBooking/index.js
+++ src/booking/CancelBooking/index.js
@@ -1,17 +1,32 @@
-import React, {useEffect} from 'react';
+import React, {Fragment} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteReservation, getReservation, getService } from '../../api'
 import moment from 'moment'
 import './style.scss'
+
 const CancelBooking = (props) => {
     const dispatch = useDispatch();
     const { reservation } = useSelector(({ global }) => global.reservation);
 
     return (
-        <div className="bookingPage">
-            <div id="reservationInformation">Are you sure you want to delete your reservation for {reservation.name} at {moment(reservation.start).format('YYYY-MM-DD HH:mm')} ?</div>
-            <div id="deleteButton" onClick={() => dispatch(deleteReservation(props.reservationId))}>Confirm</div>
-        </div>
+        <div className="paymentPageContainer">
+            {<Fragment>
+                <div class="product-card">
+                    <div class="product-details">
+                        <h1>Cancel Reservation?</h1>
+                        Are you sure you want to cancel your reservation for <b>{reservation.name}</b> at <b>{moment(reservation.start).format('YYYY-MM-DD HH:mm')}</b> ? <br /><br />
+                        <br /><br />
+                        <div className="bookingPage">
+
+                        <div id="deleteButton" onClick={() => dispatch(deleteReservation(props.reservationId))}>Confirm</div>
+                        </div>
+
+                        </div>
+                </div>
+            </Fragment>}
+        </div>        
+
     )
 }
 export default CancelBooking;
+
